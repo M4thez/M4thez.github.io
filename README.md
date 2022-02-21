f212d2cc4e99fa83dcfab9d6706dd0f95079c0c9
M4thez <namemat@gmail.com>
2022-02-22 00:25:33 +0100

    Logos/images work

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # M4thez.github.io
 
 Mateusz Boboryko's portfolio website repository, hosting via GitHub Pages.
+
+Projects logos made in Adobe Creative Cloud Express' Logo Maker
--- index.js
+++ index.js
@@ -2,59 +2,59 @@ const projects = [
   {
     "title": "Landing page project",
     "description": "responsive landing page project for a sunnyside by frontendmentor.io",
-    "image_path": "resources/sunnyside-logos/sunnyside-logos_white.png",
+    "image_path": "resources/sunnyside-logos/sunnyside_black.png",
     "links": [
-      "https://github.com/M4thez",
-      "https://google.com"
+      {
+        "repo": "https://github.com/M4thez",
+        "live": "https://google.com"
+      }
     ]
   },
   {
     "title": "Balldontlie",
     "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Debitis et culpa sequi suscipit similique laborum nisi quaerat voluptas earum deleniti exercitationem mollitia tempore dolore repellat magni corrupti quae, impedit animi at vel amet aperiam quisquam? Dolore rerum nulla architecto non!",
-    "image_path": "resources/sunnyside-logos/sunnyside-logos.jpeg",
+    "image_path": "resources/balldontlie-logos/balldontlie_black.png",
     "links": [
-      "https://github.com/M4thez",
-      "https://google.com"
+      {
+        "repo": "https://github.com/M4thez",
+        "live": "https://google.com"
+      }
     ]
 
   },
   {
     "title": "Nasa model viewer",
     "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Debitis et culpa sequi suscipit similique laborum nisi quaerat voluptas earum deleniti exercitationem mollitia tempore dolore repellat magni corrupti quae, impedit animi at vel amet aperiam quisquam? Dolore rerum nulla architecto non!",
-    "image_path": "resources/sunnyside-logos/sunnyside-logos.jpeg",
+    "image_path": "resources/nasa model viewer-logos/nasa_black.png",
     "links": [
-      "https://github.com/M4thez",
-      "https://google.com"
+      {
+        "repo": "https://github.com/M4thez",
+        "live": "https://google.com"
+      }
     ]
 
   },
   {
     "title": "Launch countdown timer",
     "description": "blalblalbal",
-    "image_path": "resources/sunnyside-logos/sunnyside-logos.jpeg",
+    "image_path": "resources/launch countdown-logos/launch_black.png",
     "links": [
-      "https://github.com/M4thez",
-      "https://google.com"
+      {
+        "repo": "https://github.com/M4thez",
+        "live": "https://google.com"
+      }
     ]
 
   },
   {
     "title": "JS Tabs",
     "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Debitis et culpa sequi suscipit similique",
-    "image_path": "",
+    "image_path": "resources/js-tabs-logos/js-tabs_black.png",
     "links": [
-      "https://github.com/M4thez",
-      "https://google.com"
-    ]
-
-  },
-  {
-    "title": "JS Tabs",
-    "description": "baasfd",
-    "image_path": "",
-    "links": [
-      "https://github.com/M4thez",
-      "https://google.com"
+      {
+        "repo": "https://github.com/M4thez",
+        "live": "https://google.com"
+      }
     ]
 
   }
@@ -63,12 +63,13 @@ const projects = [
 function renderProjects() {
   let html = ''
   projects.forEach(project => {
-    const htmlSegment = `<div>
+    const htmlSegment = `<div class="projects__just-card">
+      <img src="${project.image_path}" />
       <h3>${project.title}</h3>
       <p>${project.description}</p>
       <ul>
-        <li><a href="${project.links[0]}">Repository</a></li>
-        <li><a href="${project.links[1]}">Live site</a></li>
+        <li><a href="${project.links.repo}">Repository</a></li>
+        <li><a href="${project.links.live}">Live site</a></li>
       </ul>
     </div>`
 
