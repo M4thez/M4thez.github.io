c5643b2730d7521e72cd33fb4affb52e90905ea1
M4thez <namemat@gmail.com>
2022-02-23 00:53:39 +0100

    Images and links interaction/style

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
 const projects = [
   {
     "title": "Landing page project",
-    "description": "Responsive landing page project for a fictional company based on a challenge by <a href='https://frontendmentor.io'>frontendmentor.io</a>. The site is responsive for all commonly used screen sizes and also includes custom hover states for interactive elements.",
+    "description": "Responsive landing page project for a fictional company based on a challenge by <a href='https://frontendmentor.io'>frontendmentor.io</a>.<br><br> The site is responsive for all commonly used screen sizes and also includes custom hover states for interactive elements.",
     "image_path": "resources/sunnyside-logos/sunnyside_black.png",
-    "links": [
-      {
-        "repo": "https://github.com/M4thez/sunnyside-landing-page",
-        "live": "https://m4thez.github.io/sunnyside-landing-page"
-      }
-    ]
+    "links":
+    {
+      "repo": "https://github.com/M4thez/sunnyside-landing-page",
+      "live": "https://m4thez.github.io/sunnyside-landing-page"
+    }
+
   },
   {
     "title": "Balldontlie",
-    "description": "Single Page Application made in Vue 3. The app uses <a href='https://balldontlie.io'>balldontlie API</a> to get information about players and their stats. App is also fully dockerized with its' image available on Docker Hub.",
+    "description": "Single Page Application made in Vue 3. The app uses <a href='https://balldontlie.io'>balldontlie API</a> to get information about players and their stats. It's also fully dockerized with its' image available on Docker Hub.",
     "image_path": "resources/balldontlie-logos/balldontlie_black.png",
-    "links": [
-      {
-        "repo": "https://github.com/M4thez/balldontlie",
-        "live": "https://m4thez.github.io/balldontlie"
-      }
-    ]
+    "links":
+    {
+      "repo": "https://github.com/M4thez/balldontlie",
+      "live": "https://m4thez.github.io/balldontlie"
+    }
+
 
   },
   {
     "title": "Nasa model viewer",
-    "description": "Website with 3D models shared by official NASA repository. It's mostly a proof of concept on sharing 3D models on the web using my own solution. The viewer is made in Three.js (JavaScript library for working with 3D objects on the Web).",
+    "description": "Website with 3D models shared by official NASA repository. It's mostly a proof of concept on sharing 3D models on the web using my own solution. The viewer is made in Three.js (JavaScript library for working with 3D objects on the Web) and it's bundled with Vite.",
     "image_path": "resources/nasa model viewer-logos/nasa_black.png",
-    "links": [
-      {
-        "repo": "https://github.com/M4thez/nasa-website",
-        "live": "https://m4thez.github.io/nasa-website"
-      }
-    ]
+    "links":
+    {
+      "repo": "https://github.com/M4thez/nasa-website",
+      "live": "https://m4thez.github.io/nasa-website"
+    }
+
 
   },
   {
     "title": "Launch countdown timer",
-    "description": "Countdown timer site project based on a challenge by <a href='https://frontendmentor.io'>frontendmentor.io</a>. The project is displaying a fully animated live countdown timer that ticks down every second to a specific time. Desktop solution only.",
+    "description": "Countdown timer site project based on a challenge by <a href='https://frontendmentor.io'>frontendmentor.io</a>. The project is displaying a fully animated live countdown timer that ticks down every second to a specific time.",
     "image_path": "resources/launch countdown-logos/launch_black.png",
-    "links": [
-      {
-        "repo": "https://github.com/M4thez/launch-countdown-timer",
-        "live": "https://m4thez.github.io/launch-countdown-timer"
-      }
-    ]
+    "links":
+    {
+      "repo": "https://github.com/M4thez/launch-countdown-timer",
+      "live": "https://m4thez.github.io/launch-countdown-timer"
+    }
+
 
   },
   {
     "title": "JS Tabs",
     "description": "Simple Single Page Application project made for a University exercise. It consists of some interactive challenges that gave me some training in basic JavaScript coding.",
     "image_path": "resources/js-tabs-logos/js-tabs_black.png",
-    "links": [
-      {
-        "repo": "https://github.com/M4thez/js-tabs",
-        "live": "https://m4thez.github.io/js-tabs"
-      }
-    ]
+    "links":
+    {
+      "repo": "https://github.com/M4thez/js-tabs",
+      "live": "https://m4thez.github.io/js-tabs"
+    }
+
 
   }
 ]
@@ -64,15 +64,17 @@ function renderProjects() {
   let html = ''
   projects.forEach(project => {
     const htmlSegment = `<div class="projects__just-card">
-      <img src="${project.image_path}" />
-      <h3>${project.title}</h3>
-      <p>${project.description}</p>
-      <ul>
-        <li><a href="${project.links.repo}">Repository</a></li>
-        <li><a href="${project.links.live}">Live site</a></li>
-      </ul>
+      <a href="${project.links.live}"><img src="${project.image_path}" /></a>
+      <div class="projects__text">
+        <h3>${project.title}</h3>
+        <p>${project.description}</p>
+        <ul class="projects__text--links">
+          <li><a href="${project.links.repo}">Repository</a></li>
+          <li><a href="${project.links.live}">Live site</a></li>
+        </ul>
+      </div>
     </div>`
-
+    console.log(project)
     html += htmlSegment
   })
   const container = document.querySelector('#projects__cards')
