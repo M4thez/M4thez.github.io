ecaee5cbfaa726ef881a9b5338b3824c7c7aa3b2
M4thez <namemat@gmail.com>
2025-06-16 21:40:45 +0200

    Added some Lighthouse fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,20 +3,16 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <ul>
-        <div className="links-group">
-          <li>
-            <a href="#welcome">Home</a>
-          </li>
-        </div>
-        <div className="links-group">
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </div>
+      <ul className="navbar-links">
+        <li>
+          <a href="#welcome">Home</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
     </nav>
   );
